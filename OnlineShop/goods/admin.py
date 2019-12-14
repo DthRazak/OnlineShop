fb689181f71e94c6c7198c2297f8149bb1123381
dthrazak <vovamilchan@gmail.com>
2019-12-14 14:08:26 +0200

    Added items to the database

--- OnlineShop/goods/admin.py
+++ OnlineShop/goods/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from goods.models import Good, GoodImage
 # Register your models here.
+
+admin.site.register(Good)
+admin.site.register(GoodImage)
--- OnlineShop/main/views.py
+++ OnlineShop/main/views.py
@@ -1,9 +1,15 @@
 from django.views.generic.base import TemplateView
+from categories.models import Category
+from goods.models import Good
 
 
 class MainPageView(TemplateView):
     template_name = "mainpage.html"
+    categories = Category.objects.all()
+    goods = Good.objects.filter(featured=True)[:3]
 
     def get_context_data(self, **kwargs):
         context = super(MainPageView, self).get_context_data(**kwargs)
+        context["categories"] = self.categories
+        context["goods"] = self.goods
         return context
