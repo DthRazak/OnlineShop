960cdb1a8bac31f23ea42327ab180a7d89151dba
dthrazak <vovamilchan@gmail.com>
2019-12-16 17:08:29 +0200

    Added pagination and filtering

--- OnlineShop/goods/urls.py
+++ OnlineShop/goods/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from goods.views import GoodDetailView
+from goods.views import GoodDetailView, catalog
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)$', GoodDetailView.as_view(), name="good"),
+    url(r'^catalog$', catalog, name="catalog"),
 ]
--- OnlineShop/goods/views.py
+++ OnlineShop/goods/views.py
@@ -1,6 +1,9 @@
+from django.core.paginator import Paginator
 from django.http import Http404
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from categories.models import Category
 from goods.models import Good
 
 
@@ -15,3 +18,45 @@ class GoodDetailView(TemplateView):
             raise Http404("Good does not exist")
 
         return context
+
+
+def catalog(request):
+    context = dict()
+    goods = Good.objects.all()[::1]
+
+    category = request.GET.get('category')
+    if category is not None:
+        goods = list(filter(lambda g: g.category.name == category, goods))
+
+    price = request.GET.get('price')
+    if price is not None:
+        if price == 'lower':
+            goods.sort(key=lambda g: g.price)
+        else:
+            goods.sort(key=lambda g: g.price, reverse=True)
+
+    recommend = request.GET.get('recommend')
+    if recommend is not None:
+        if recommend == 'true':
+            goods = list(filter(lambda g: g.featured, goods))
+
+    in_stock = request.GET.get('in_stock')
+    if in_stock is not None:
+        if in_stock == 'true':
+            goods = list(filter(lambda g: g.in_stock, goods))
+
+    name = request.GET.get('search')
+    if name is not None:
+        goods = list(filter(lambda g: g.name.lower().startswith(name.lower()), goods))
+
+    categories = Category.objects.all()
+    paginator = Paginator(goods, 6)
+    page = request.GET.get('page')
+    good_list = paginator.get_page(page)
+
+    context['goods'] = goods[:3]
+    context['good_list'] = good_list
+    context['categories'] = categories
+    context['page_range'] = list(paginator.page_range)
+
+    return render(request, 'catalog.html', context)
